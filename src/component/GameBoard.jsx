014d0b7c37ea336af9efb6bb286f6066a9023e0f
MuradIbrahimov <104825680+MuradIbrahimov@users.noreply.github.com>
2025-07-19 13:47:00 +0400

    double click bug fix

--- src/component/GameBoard.jsx
+++ src/component/GameBoard.jsx
@@ -22,7 +22,7 @@ for (const turn of turns){
         {gameBoard.map((row,rowIndex) => <li key={rowIndex}>
         <ol>
             {row.map((playerSymbol,cellIndex) => <li key={cellIndex}>
-                <button className="box" onClick={() => onSelectSquare(rowIndex,cellIndex)}>
+                <button disabled={playerSymbol !==null} className="box" onClick={() => onSelectSquare(rowIndex,cellIndex)}>
                     <span>{playerSymbol}</span>
                 </button>
             </li>)}
